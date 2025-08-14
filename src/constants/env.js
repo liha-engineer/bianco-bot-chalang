49bca0db9edbd222c1a848de2a9b1d0e271e16e5
liha-engineer <liha.engineer@gmail.com>
2025-08-14 17:54:17 +0900

    modify: 환경변수 묶음처리

--- src/constants/env.js
+++ src/constants/env.js
@@ -2,9 +2,12 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-export const YES_TOKEN = process.env.YES_TOKEN 
-export const WAITING_VOICE_CHANNEL_ID = process.env.WAITING_VOICE_CHANNEL_ID 
-export const DEFAULT_ROLE_ID = process.env.DEFAULT_ROLE_ID 
-export const LOG_CHANNEL_ID = process.env.LOG_CHANNEL_ID 
-export const ANNOUNCEMENT_CHANNEL_ID = process.env.ANNOUNCEMENT_CHANNEL_ID 
-export const GUIDE_CHANNEL_ID = process.env.GUIDE_CHANNEL_ID 
+export const CONSTANTS = {
+YES_TOKEN: process.env.CHALANG_TOKEN, 
+WAITING_VOICE_CHANNEL_ID: process.env.WAITING_VOICE_CHANNEL_ID,
+DEFAULT_ROLE_ID: process.env.DEFAULT_ROLE_ID, 
+LOG_CHANNEL_ID: process.env.LOG_CHANNEL_ID, 
+ANNOUNCEMENT_CHANNEL_ID: process.env.ANNOUNCEMENT_CHANNEL_ID, 
+GUIDE_CHANNEL_ID: process.env.GUIDE_CHANNEL_ID, 
+}
+
